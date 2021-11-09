6f727b076b338856b6f27ede761f4ba7f3b4de2e
kimye-seul <kimyeseul9606@gmail.com>
2021-11-09 17:20:51 +0900

    푸터 작업중

--- src/assets/app/js/common-pub.js
+++ src/assets/app/js/common-pub.js
@@ -1,5 +1,19 @@
 var commonPub = {
 	common : function(){
+		setTimeout(function(){
+			commonPub.footerFamilySite();
 
+		},350)
 	},
+
+	// 패밀리사이트
+	footerFamilySite : function() {
+		var btnFamily = $('#footer .btn_family');
+		var listFamily = $('#footer').find('.family_list');
+		btnFamily.on('click',function(){
+			$(this).toggleClass('on');
+			listFamily.slideToggle(300);
+		});
+		listFamily.find('ul').mCustomScrollbar();
+	},	
 }
