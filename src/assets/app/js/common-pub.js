a78f55314ef52079122a78045dfa2afbb986abcc
kimye-seul <kimyeseul9606@gmail.com>
2021-11-12 11:44:07 +0900

    작업중

--- src/assets/app/js/common-pub.js
+++ src/assets/app/js/common-pub.js
@@ -1,19 +1,37 @@
 var commonPub = {
 	common : function(){
 		setTimeout(function(){
-			commonPub.footerFamilySite();
+			commonPub.footer.footerFamilySite();
+			commonPub.footer.pageTop();
 
 		},350)
 	},
 
-	// 패밀리사이트
-	footerFamilySite : function() {
-		var btnFamily = $('#footer .btn_family');
-		var listFamily = $('#footer').find('.family_list');
-		btnFamily.on('click',function(){
-			$(this).toggleClass('on');
-			listFamily.slideToggle(300);
-		});
-		listFamily.find('ul').mCustomScrollbar();
-	},	
+	//header
+	header : {
+
+	},
+
+	//footer
+	footer : {
+		//탑버튼
+		pageTop : function(){
+			$('#footer .btn_top').off('click').on('click',function(){
+				$('html,body').animate({
+					scrollTop : 0
+				},800)
+			})
+		},
+
+		// 패밀리사이트
+		footerFamilySite : function() {
+			var btnFamily = $('#footer .btn_family');
+			var listFamily = $('#footer').find('.family_list');
+			btnFamily.on('click',function(){
+				$(this).toggleClass('on');
+				listFamily.slideToggle(300);
+			});
+			listFamily.find('ul').mCustomScrollbar();
+		},	
+	}
 }
